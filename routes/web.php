8084fa96178968e49866884001878ad406e03b21
3films-c-izumi <c-izumi@3films.com>
2025-09-01 18:42:25 +0900

    reactインストール

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\FormController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/form', [FormController::class, 'ShowForm'])->name('form');
